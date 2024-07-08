459afa013a76eeaa96730fe470200942bc527d9f
luisEduardoDev-gcol <luiseduardosinisterramontano@gmail.com>
2024-07-07 22:30:20 -0500

    Commint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TablaCursosComponent } from './tabla-cursos/tabla-cursos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablaCursosComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tabla-cursos/tabla-cursos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-cursos',
+  templateUrl: './tabla-cursos.component.html',
+  styleUrls: ['./tabla-cursos.component.css']
+})
+export class TablaCursosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
